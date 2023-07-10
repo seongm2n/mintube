7eefb425fbf7828ce19d53ef69a155092af94930
seongmin <seongm22n@gmail.com>
2023-07-11 01:42:56 +0900

    https 수정

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -18,7 +18,7 @@ function VideoDetail() {
 					type='text/html'
 					width='100%'
 					height='640'
-					src={`http://www.youtube.com/embed/${video.id}`}
+					src={`https://www.youtube.com/embed/${video.id}`}
 					frameborder='0'
 					title={title}
 				/>
