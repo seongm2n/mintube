626cebc2065b1391431eaeee61c941c758cef0fc
seongmin <seongm22n@gmail.com>
2023-07-10 19:00:06 +0900

    라이브러리 사용해서 timeago 해주기

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,35 +1,8 @@
 import React from 'react';
+import { formatAgo } from '../../util/date';
 
 function VideoCard({ video }) {
 	const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
-	const publishedAtDate = new Date(publishedAt);
-	const currentDate = new Date();
-
-	const timeDiff = currentDate.getTime() - publishedAtDate.getTime();
-	const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-	const monthDiff =
-		currentDate.getMonth() -
-		publishedAtDate.getMonth() +
-		12 * (currentDate.getFullYear() - publishedAtDate.getFullYear());
-	let timeAgo;
-
-	if (daysDiff < 1) {
-		const hourDiff = Math.floor(timeDiff / (100 * 60 * 60));
-		if (hourDiff < 1) {
-			const minuteDiff = Math.floor(timeDiff / (1000 * 60));
-			timeAgo = `${minuteDiff}${minuteDiff !== 1 ? '분' : ''}전`;
-		} else {
-			timeAgo = `${hourDiff}${hourDiff !== 1 ? '시간' : ''}전`;
-		}
-	} else if (daysDiff < 7) {
-		timeAgo = `${daysDiff}${daysDiff !== 1 ? '일' : ''}전`;
-	} else if (monthDiff < 1) {
-		timeAgo = `${Math.floor(daysDiff / 7)}${
-			Math.floor(daysDiff / 7) !== 1 ? '주' : ''
-		}전`;
-	} else {
-		timeAgo = `${monthDiff}${monthDiff !== 1 ? '달' : ''}전`;
-	}
 
 	return (
 		<>
@@ -43,7 +16,7 @@ function VideoCard({ video }) {
 				<div style={{ display: 'flex', flexDirection: 'column' }}>
 					<p style={{ overflow: 'hidden' }}>{title}</p>
 					<p>{channelTitle}</p>
-					<p>{timeAgo}</p>
+					<p>{formatAgo(publishedAt, 'ko')}</p>
 				</div>
 			</li>
 		</>
